912da9aa49258c29ee657199b089d5263dd8ed94
Wandi <wandifrog@gmail.com>
2020-11-20 19:59:01 +0700

    chore: success scraping

--- index.js
+++ index.js
@@ -1,52 +1,77 @@
 const puppeteer = require('puppeteer');
 const jsonfile = require('jsonfile');
 const fs = require('fs');
+const cheerio = require('cheerio');
 
 const RESULTS_FOLDER_PATH = './scraping-results/'
 const CONFIG_FILE = './config.json'
 
-jsonfile.spaces = 2
-
-const fileName = `${RESULTS_FOLDER_PATH}/data.json`
-const obj = { name: 'JP' }
+const options = {
+  headless: true,
+  // slowMo: 250
+}
+const episodeCounter = 10
 
 async function main() {
+  // const counter = await getCounterValue()
+  startBot()
+}
+
+async function startBot() {
+  const url = 'view-source:https://bacakomik.co/one-piece-chapter-001-bahasa-indonesia/'
+  const url2 = 'https://bacakomik.co/one-piece-chapter-001-bahasa-indonesia/'
+
+  const browser = await puppeteer.launch(options);
+  const page = await browser.newPage();
+
+  // await page.goto(url, { waitUntil: 'networkidle2' })
+  const bacakomik = await page.goto(url, { waitUntil: 'networkidle2' })
+
   
-  console.log(1)
-  await getCounterValue()
-  console.log(2)
-  // startBot()
+  // cheerio
+  // const asd = cheerio.load(bacakomik.text())  
+  // console.log(222, asd('div', '#chimg'))
+  
+  // await page.waitForSelector('#chimg')
+  // const result = await page.$$('#chimg')
+  try { 
+    console.log(await bacakomik.text())
+    const html = bacakomik.text()
+    fs.writeFileSync('test.html', await bacakomik.text())
+
+
+
+    // let data = await bacakomik.evaluate(() => {
+    //   const title = document
+    //   return title
+    // })
+    // console.log(123, data)
+    
+  } catch (error) {
+    console.log('CATCH')
+    await browser.close()
+  }
+
+
+  await browser.close();
+}
+
+function delay(time) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time)
+  });
 }
 
 function getCounterValue() {
   return new Promise(async resolve => {
     const { counter, ...configJson } = await jsonfile.readFile(CONFIG_FILE)
-  
     const newConfigJson = {
       ...configJson,
       counter: counter + 1
     }
-
-    console.log(configJson)
     jsonfile.writeFileSync(CONFIG_FILE, newConfigJson)
     resolve(counter)
   })
-}
-
-async function startBot() {
-  const url = 'view-source:https://bacakomik.co/one-piece-chapter-001-bahasa-indonesia/'
-  const url2 = 'https://www.youtube.com/watch?v=TzZ3YOUhCxo'
-
-  const browser = await puppeteer.launch({ headless: false, slowMo: 250 });
-  // const browser = await puppeteer.launch();
-  // const page = await browser.newPage();
-  browser.goto(url)
-  // await browser.waitFor(6000)
-  // const response = await page.goto(url);
-  // console.log(await response.text())
-  // await page.screenshot({path: 'example.png'});
-
-  await browser.close();
 }
 
 main()
